51f8fc78a82ee19db404484b129efaa1e1c95207
Kyle <kyleandrewmckee@gmail.com>
2019-05-08 10:49:23 -0700

    added addWord method to the Trie class and corresponding tests

--- trie.js
+++ trie.js
@@ -10,6 +10,19 @@ class Trie {
     constructor() {
         this.root = new TrieNode('*');
     }
+
+    addWord(word) {
+        let currentNode = this.root;
+        let endOfWord = false;
+        for (let i = 0; i < word.length; i++) {
+            endOfWord = (i === word.length - 1) ? true : false;
+            if (!(word[i] in currentNode.children)) {
+                currentNode.children[word[i]] = new TrieNode(word[i], endOfWord);
+            }
+            currentNode = currentNode.children[word[i]];
+            currentNode.endOfWord = endOfWord;
+        }
+    }
 }
 
 
--- trie.test.js
+++ trie.test.js
@@ -10,3 +10,64 @@ describe('Newly created trie', () => {
         expect(trie.root instanceof TrieNode).toBeTruthy();
     });
 });
+
+describe('When adding words to Trie using the addWord method', () => {
+    let trie = new Trie;
+    trie.addWord('PIE');
+    trie.addWord('PI');
+    trie.addWord('GAMMA');
+
+    // Define nodes initially since jest runs tests in parallel
+    let root = trie.root;
+    let pChild = root.children['P'];
+    let iChild = pChild.children['I'];
+    let eChild = iChild.children['E'];
+
+    let gChild = root.children['G'];
+    let aChild1 = gChild.children['A'];
+    let mChild1 = aChild1.children['M'];
+    let mChild2 = mChild1.children['M'];
+    let aChild2 = mChild2.children['A'];
+
+    
+    test('the root node should have P as a child', () => {
+        expect('P' in root.children).toBeTruthy();
+    });
+
+    test('the root node should not have I as a child', () => {
+        expect('I' in root.children).toBeFalsy();
+    });
+
+    test('the child of P should have I as a child', () => {
+        expect('I' in pChild.children).toBeTruthy();
+    });
+
+    test('the I node should be the end of the word PI', () => {
+        expect(iChild.endOfWord).toBeTruthy();
+    });
+
+    test('the E node should be the end of the word PIE', () => {
+        expect(eChild.endOfWord).toBeTruthy();
+    });
+
+    test('M nodes should not be the end of the word GAMMA', () => {
+        expect(mChild1.endOfWord).toBeFalsy();
+        expect(mChild2.endOfWord).toBeFalsy();
+    });
+
+    test('the child of A should contain M', () => {
+        expect('M' in aChild1.children).toBeTruthy();
+    });
+
+    test('the child of A should not contain I', () => {
+        expect('I' in aChild1.children).toBeFalsy();
+    });
+
+    test('The first A node of GAMMA should not be the end of the word', () => {
+        expect(aChild1.endOfWord).toBeFalsy();
+    });
+    
+    test('The second A node of GAMMA should be the end of the word', () => {
+        expect(aChild2.endOfWord).toBeTruthy();
+    });
+});
